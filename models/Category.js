f23ba213a545b0eeef013b31cae57684c21474f1
Simon-Xu-Lan <xulan20907@gmail.com>
2022-05-24 09:20:47 -0600

    modified

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,17 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection.js');
+const sequelize = require('../config/connection');
 
 class Category extends Model {}
 
 Category.init(
   {
-    // id:
-    // {
-    //   type: DataTypes.INTEGER,
-    //   allowNull: false,
-    //   primaryKey: true,
-    //   autoIncrement: true
-    // },
-    // define columns
+    id:
+    {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false
